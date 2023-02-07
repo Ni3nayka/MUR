55e601bbae591fbdda9cc8a24e7563a5be6ba665
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2023-02-07 13:21:44 +0300

    complete arduino i2c protocol

--- src/arduino_motor/arduino_motor.ino
+++ src/arduino_motor/arduino_motor.ino
@@ -10,30 +10,108 @@
 #include "I2C_config.h"
 #include <Wire.h>
 
+int device_mode = 0;
+/*
+ * 0 - no (error)
+ * 1 - motor
+ * 2 - servo
+ * 3 - sensor
+ */
+int device_number = 0;
+int device_input_counter = 0;
+
+int servo_angle[2] = {90,90};
+int motor_speed[2] = {0,0};
+int motor_distanse[2] = {0,0};
+
+#define DEBUG
+
 void setup() {
   Wire.begin(I2C_ADRESS); 
   Wire.setClock(I2C_CLOCK);
   Wire.onReceive(receiveEvent); /* регистрируем полученное событие */
   Wire.onRequest(requestEvent); /* регистрируем запрошенное событие */
-  Serial.begin(115200);         /* открываем серийный порт для дебаггинга */
+  Serial.begin(115200);
 }
 
 void loop() {
-  //delay(100);
+  update_servo();
+  update_motor();
 }
 
-// Функция для извлечения любых принимаемых данных от мастера на шину
+void  update_servo() {
+  
+}
+
+void  update_motor() {
+  
+}
+
 void receiveEvent(int howMany) {
+  device_input_counter = 0;
+  device_mode = 0;
+  device_number = 0;
   while (0 < Wire.available()) {
-    int c = Wire.read();      /* получаем байт как символ*/
-    Serial.print(int(c));      /* выводим символ в серийный порт */
-    Serial.print(" ");
+    int c = Wire.read(); 
+    #ifdef DEBUG
+      Serial.print(int(c)); 
+      Serial.print(" ");
+    #endif
+    if (device_input_counter==0) {
+      if (I2C_SYMBOL_MOTOR(c)) {
+        device_mode = 1;
+        device_number = I2C_READ_NUMBER_MOTOR(c);
+      }
+      else if (I2C_SYMBOL_SERVO(c)) {
+        device_mode = 2;
+        device_number = I2C_READ_NUMBER_SERVO(c);
+      }
+      else if (I2C_SYMBOL_SENSOR(c)) {
+        device_mode = 3;
+        device_number = I2C_READ_NUMBER_SENSOR(c);
+      }
+      else device_mode = 0;
+    }
+    else if (device_mode>0) {
+      if (device_input_counter==1) {
+        if (device_mode==1) motor_speed[device_number-1] = I2C_READ_SPEED_MOTOR(c);
+        else if (device_mode==2) servo_angle[device_number-1] = c;
+        else device_mode = 0;
+      }
+      else if (device_input_counter==2) {
+        if (device_mode==1) motor_distanse[device_number-1] = c;
+        else device_mode = 0;
+      }
+      else device_mode = 0;
+    }
+    device_input_counter++;
   }
-  Serial.println();             /* переходим на новую строку */
+  #ifdef DEBUG
+    Serial.println(); 
+    print_data();
+  #endif
+  if (device_mode==0) Serial.println("ERROR: the message was received with an error");
 }
 
-// Функция для извлечения любых отправляемых данных от мастера на шину
+void print_data() {
+  Serial.print("motor_speed: ");
+  Serial.print(motor_speed[0]);
+  Serial.print(" ");
+  Serial.print(motor_speed[1]);
+  Serial.print("   motor_distanse: ");
+  Serial.print(motor_distanse[0]);
+  Serial.print(" ");
+  Serial.print(motor_distanse[1]);
+  Serial.print("   servo_angle: ");
+  Serial.print(servo_angle[0]);
+  Serial.print(" ");
+  Serial.print(servo_angle[1]);
+  Serial.println();
+}
+
 void requestEvent() {
-  //Wire.write("Hello NodeMCU");  /* отправляем по запросу строку "Hello NodeMCU" */
-  Wire.write(SYMBOL_OK);
+  if (device_mode==1 || device_mode==2) Wire.write(SYMBOL_OK);
+  else if (device_mode==3) Wire.write(char(map(analogRead(device_number),0,1023,0,250)));
+  else Wire.write(SYMBOL_ERROR);
+  device_mode = 0;
 }
